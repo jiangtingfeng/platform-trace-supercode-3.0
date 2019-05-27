4010cf1c1186eca747a62dee0289a9dd2971ccc0
wangzhaoqing <wangzhaoqing@app315.net>
2019-05-27 14:29:48 +0800

    fixbug

--- src/main/java/com/jgw/supercodeplatform/trace/dao/mapper1/zaoyangpeach/StoragePlaceExMapper.java
+++ src/main/java/com/jgw/supercodeplatform/trace/dao/mapper1/zaoyangpeach/StoragePlaceExMapper.java
@@ -19,7 +19,7 @@ public interface StoragePlaceExMapper extends StoragePlaceMapper {
                     "SELECT COUNT(1) FROM zaoyang_storageplace a"
                     +startWhere
                     + " <if test='organizationId !=null and organizationId != &apos;&apos; '>  AND a.OrganizationId = #{organizationId} </if> "
-                    + " <if test='disableFlag !=null  '>  DisableFlag = #{disableFlag} </if> "
+                    + " <if test='disableFlag !=null  '> AND DisableFlag = #{disableFlag} </if> "
                     + " <if test='search !=null and search != &apos;&apos; '> AND ( a.PlaceName LIKE CONCAT('%',#{search},'%') or a.SortingPlaceName LIKE CONCAT('%',#{search},'%') or a.PlaceNumber LIKE CONCAT('%',#{search},'%')or a.PlaceStaff LIKE CONCAT('%',#{search},'%') )</if> "
                     +endWhere
                     +page
@@ -33,7 +33,7 @@ public interface StoragePlaceExMapper extends StoragePlaceMapper {
                     "select * from zaoyang_storageplace a"
                     +startWhere
                     + " <if test='organizationId !=null and organizationId != &apos;&apos; '>  AND a.OrganizationId = #{organizationId} </if> "
-                    + " <if test='disableFlag !=null  '>  DisableFlag = #{disableFlag} </if> "
+                    + " <if test='disableFlag !=null  '> AND DisableFlag = #{disableFlag} </if> "
                     + " <if test='search !=null and search != &apos;&apos; '> AND ( a.PlaceName LIKE CONCAT('%',#{search},'%') or a.SortingPlaceName LIKE CONCAT('%',#{search},'%') or a.PlaceNumber LIKE CONCAT('%',#{search},'%')or a.PlaceStaff LIKE CONCAT('%',#{search},'%') )</if> "
                     +endWhere
                     +orderBy
