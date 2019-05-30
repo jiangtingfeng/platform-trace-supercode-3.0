64214c97ea674df2cc7809ba0ce99ea2bbb721cd
wangzhaoqing <wangzhaoqing@app315.net>
2019-05-30 10:22:39 +0800

    增加请求转发功能，/supercode下接口转发调用超级码2.0接口

---
+++ src/main/java/com/jgw/supercodeplatform/trace/service/supercode/DataService.java
@@ -0,0 +1,48 @@
+package com.jgw.supercodeplatform.trace.service.supercode;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Service
+public class DataService {
+
+    @Value("${rest.supercode.url}")
+    private String superCodeUrl;
+
+    @Value("${rest.supercode.token}")
+    private String superCodeToken;
+
+    RestTemplate restTemplate;
+
+    public String getForObject(String functionId,String queryString) throws Exception {
+        restTemplate=new RestTemplate();
+        String url=String.format( "%s/JsonDataService.ashx?Token=%s&function=%s",superCodeUrl,superCodeToken,functionId);
+        if(StringUtils.isNotEmpty(queryString)){
+            url+="&"+queryString;
+        }
+        String responseText = restTemplate.getForObject(url,String.class);
+        return responseText;
+    }
+
+
+    /*
+        if (StringUtils.isEmpty(token)){
+            String responseText= restTemplate.getForObject("http://interface.kf315.net/JsonDataService.ashx?function=CreateToken",String.class);
+
+            JSONObject jsonObject= JSONObject.parseObject(responseText, JSONObject.class);
+            JSONObject data= (JSONObject)jsonObject.get("Data");
+            token= data.get("Token").toString();
+
+            String url=String.format( "http://interface.kf315.net/JsonDataService.ashx?function=BindUserAndToken&Token=%s&LoginName=%s&Password=%s",token,"ceshi","123456");
+            responseText= restTemplate.getForObject(url,String.class);
+
+        }
+    * */
+
+}
